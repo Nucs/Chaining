a8c489d33e498f5ef8018ca42d48805fabfb11fa
Eli <elibelash@gmail.com>
2018-08-27 20:16:17 +0300

    Added more complex example.
    Signed-off-by: Eli <elibelash@gmail.com>

--- example/Program.cs
+++ example/Program.cs
@@ -1,33 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using nucs.Chaining.Extensions;
 
 namespace nucs.Chaining.Example {
     class Program {
         static void Main(string[] args) {
+
+            bool hascalled = false;
+
             var statemachine = Chain.Build(c => {
-                //initialize the states
-                Console.WriteLine("Welcome;");
-                
-                int a = int.MaxValue;
-                int b = int.MaxValue;
+                //initialize machine variables
+                c.NameScript("Root"); //names current script root.
+                hascalled = false;
+                Console.WriteLine("Machine initialized;");
 
-                //add independent script that can be called at any time:
-                c.NameScript("End", _ => {
-                    Console.WriteLine($"Finished, {a}*{b}={a*b}");
-                    return c.Complete();
-                });
+                return _1 => {
+                    c.NameScript("Get number"); //names current script root.
+                    Console.Write("Enter number to call: ");
+                    var number = Console.ReadLine();
+                    if (number==null || !number.All(Char.IsDigit)) { //if input is not all digits, repeat script on next pulse.
+                        Console.WriteLine($"{number} is invalid phone number.");
+                        return null; //returning null singals to repeat this script on next pulse, equivalent to c.Repeat.
+                    }
 
-                //continue immediatly to next state without waiting for an other pulse:
-                return c.Continue(_ => {
-                    
-                });
+                    hascalled = true;
+
+                    //continue immediatly to next state without waiting for an other pulse:
+                    return c.Continue(_2 => {
+                        c.NameScript("Wait for number");
+
+                        var sw = new Stopwatch();
+                        Console.WriteLine($"Calling {number}...");
+                        sw.Start();
+                        //continue immediatly to current script.
+                        return c.Continue(__ => {
+                            //do actual call here
+                            Thread.Sleep(1500);
+                            sw.Stop();
+                            Console.WriteLine($"Call finished after {sw.Elapsed.TotalSeconds:0.##}s.");
+                            return c.BackwardsTo("Wait for number"); //walks backwards in callstack to the script named "Wait for number"
+                        });
+                    });
+                };
             });
 
-            statemachine.Pulse();
+            statemachine.Pulse(); //first pulse to initialize machine.
+
+            while(!hascalled)
+                statemachine.Pulse();
 
             Console.WriteLine();
             Console.WriteLine("Exited State Machine.");
