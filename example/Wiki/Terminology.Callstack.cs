8eb51d9380434a0caefc1f81b453a0970de7c8cd
Eli <elibelash@gmail.com>
2018-08-28 11:34:09 +0300

    Fixed callstack
    Signed-off-by: Eli <elibelash@gmail.com>

--- example/Wiki/Terminology.Callstack.cs
+++ example/Wiki/Terminology.Callstack.cs
@@ -1,28 +1,31 @@
 ﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace nucs.Chaining.Example.Wiki
-{
+namespace nucs.Chaining.Example.Wiki {
     [TestClass]
     public class Teminology_Callstack {
         [TestMethod]
         public void X() {
-            var chain = Chain.Build(c => { 
+            var chain = Chain.Build(c => {
                 //c.StackSize == 1
-                //script 1
+                //script 1 - initialization
                 int i = 1;
-                if (i == 2)
-                    return c.Complete();
-                return _c => { 
+                return c.Continue(_ => {
                     //c.StackSize == 2
-                    //script 2
-                    i++;
-                    return c.Backwards(1); // goes back to script 1.
-                };
+                    //script 2 - condition
+                    if (i == 2)
+                        return c.Complete();
+                    return _c => {
+                        //c.StackSize == 3
+                        //script 3 - action
+                        i++;
+                        return c.Backwards(1); // goes back to script 2.
+                    };
+                });
             });
 
-            chain.Pulse(); 
-            chain.Pulse(); 
+            chain.Pulse();
+            chain.Pulse();
             chain.Pulse();
             chain.Completed.Should().BeTrue();
         }
